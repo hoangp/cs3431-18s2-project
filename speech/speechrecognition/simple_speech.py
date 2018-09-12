5d48b49572d419c17c4d96b89c2643fc8c29511c
missmint27 <zyk493122273@163.com>
2018-09-12 20:42:08 +1000

    #2 in google_speech_api file, detect from file is works fine, but from microphone, there is a bug in my computer. Speechrecognition is a library which can use google_cloud_api

---
+++ speech/speechrecognition/simple_speech.py
@@ -0,0 +1,23 @@
+ # -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+
+import speech_recognition as sr  
+   
+# obtain audio from the microphone  
+r = sr.Recognizer()  
+with sr.Microphone() as source:  
+   print("Please wait. Calibrating microphone...")  
+   # listen for 5 seconds and create the ambient noise energy level  
+   r.adjust_for_ambient_noise(source, duration=5)  
+   print("Say something!")  
+   audio = r.listen(source)  
+   
+# recognize speech using Sphinx  
+try:  
+    #print("Sphinx thinks you said '" + r.recognize_sphinx(audio) + "'")
+    print("Sphinx thinks you said '" + r.recognize_google(audio) + "'")  
+except sr.UnknownValueError:  
+    print("Sphinx could not understand audio")  
+except sr.RequestError as e:  
+    print("Sphinx error; {0}".format(e))  
