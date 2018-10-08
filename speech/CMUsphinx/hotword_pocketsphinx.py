6bc7eae20a03fce89e0cfdb8bd8dcbba97e40f74
missmint27 <zyk493122273@163.com>
2018-10-08 16:00:42 +1100

    #2 CMU

--- speech/CMUsphinx/hotword_pocketsphinx.py
+++ speech/CMUsphinx/hotword_pocketsphinx.py
@@ -1,29 +1,29 @@
 from pocketsphinx import *
 import pyaudio
 
-pocketsphinx_dir = os.path.dirname(pocketsphinx.__file__)
-model_dir = os.path.join(pocketsphinx_dir, 'model')
+model_dir = os.path.join(os.path.dirname(pocketsphinx.__file__), 'model')
 
 config = pocketsphinx.Decoder.default_config()
 config.set_string('-hmm', os.path.join(model_dir, 'en-us'))
 config.set_string('-keyphrase', 'hello')
-config.set_string('-dict', os.path.join(model_dir, 'en-us/cmudict-en-us.dict'))
+config.set_string('-dict', os.path.join(model_dir, 'cmudict-en-us.dict'))
 config.set_float('-kws_threshold', 1e-5)
+decoder = Decoder(config)
 
 p = pyaudio.PyAudio()
 
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=20480)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
 stream.start_stream()
 
+decoder.start_utt()
 while True:
-    buf = stream.read(1024)
-    if buf:
-        decoder.process_raw(buff)
+    buff = stream.read(1024)
+    if buff:
+        decoder.process_raw(buff,False,False)
     else:
-        break;
+        break
 
-if decoder.hyp() is not None:
-    print("Hotword Detected")
-    decoder.end_utt()
-    start_speech_recognition()
-    decoder.start_utt()
+    if decoder.hyp() is not None and decoder.hyp().hypstr == 'hello':
+        decoder.end_utt()
+        print("Hotword Detected")
+        decoder.start_utt()
