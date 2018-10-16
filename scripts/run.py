d1f8445f8e08d62aac14aaa8563a07af0b00a26b
Hoang Pham <hoang.unsw@gmail.com>
2018-10-16 11:41:54 +1100

    update who, move not working yet

--- scripts/run.py
+++ scripts/run.py
@@ -7,6 +7,11 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 import pyttsx
+from geometry_msgs.msg import Twist
+import math
+
+PI = math.pi
+BOX_MIN = 5
 
 POSE_BODY_25_BODY_PARTS = {
     0:  "Nose",
@@ -40,9 +45,32 @@ POSE_BODY_25_BODY_PARTS = {
 POSE_BODY_25_BODY_PARTS_CONV = {name: index for index, name in POSE_BODY_25_BODY_PARTS.items()}
 pb = POSE_BODY_25_BODY_PARTS_CONV
 
-def draw_rectangle(img, rect):
+def genTwist(lx, ly, lz, ax, ay, az):
+    twist = Twist()
+    twist.linear.x = lx
+    twist.linear.y = ly
+    twist.linear.z = lz
+    twist.angular.x = ax
+    twist.angular.y = ay
+    twist.angular.z = az
+    return twist
+
+def draw_rectangle(img, rect, text=None):
+    if text == "?":
+        color = (0, 255, 255)
+    else:
+        color = (0, 255, 0)
     (x, y, w, h) = rect
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+    if text:
+        draw_text(img, text, x+5, y+20, color)
+
+def draw_text(img, text, x, y, color):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, color, 2)
+
+def draw_status(img, known, unknown):
+    text = "known=" + str(known) + " unknown=" + str(unknown) + " total=" + str(known+unknown)
+    cv2.putText(img, text, (5, 20), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
 
 def is_valid_point(p):
     #return (p[0]>100 and p[0]<540 and p[1]>50 and p[1] < 430)
@@ -73,7 +101,8 @@ def hist_similar(lhist, rhist):
 def get_sim(unknowbody_pic, know_body):
     testHist, _ = get_histGBR(know_body)
     h,w,_ = know_body.shape
-    if h==0 or w==0:
+    h2,w2,_ = unknowbody_pic.shape
+    if h2<BOX_MIN or w2<=BOX_MIN:
         return 0
     unknowbody_pic = cv2.resize(unknowbody_pic, (w,h))
     #print know_body.shape, type(know_body), unknowbody_pic.shape, type(unknowbody_pic)
@@ -102,6 +131,7 @@ class Run:
 
         self.voice_pub = rospy.Publisher('pr/voice', String, queue_size=10)
         self.display_pub = rospy.Publisher('pr/display', Image, queue_size=10)
+        self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 
         rospy.Subscriber('pr/camera', Image, self.callback_camera)
         rospy.Subscriber('pr/op_25kps', String, self.callback_25kpts)
@@ -183,7 +213,7 @@ class Run:
             self.command = cmd[0]
             self.name = cmd[1]
         else:
-            self.command = command
+            self.command = command.data
 
     def filter_kps(self, kpts, filter):
         kps = []
@@ -222,7 +252,7 @@ class Run:
         return [self.get_box(kpts, idx, required_all_points) for kpts in self.kpts]
 
     def get_personboxes(self, required_all_points):
-        idx = {0,1,2,5,8,9,12,10,13,15,16,17,18}
+        idx = range(25)
         #print("bodybox")
         #self.show_kps(idx)
         return [scale_box(self.get_box(kpts, idx, required_all_points),ymin_factor=0.90) for kpts in self.kpts]
@@ -241,31 +271,34 @@ class Run:
     def list_cmd(self):
         if self.data:
             names = self.data.keys()
-            text = "there are " + str(len(names)) + " people: "
+            text = "In my memory, there are " + str(len(names)) + " people: "
             for name in names:
                 text = text + ", " + name
             self.taskdone(text)
-            self.display_cam = True
         else:
-            self.taskdone("there's no ones in the database")
-            self.display_cam = True
+            self.taskdone("there's no ones in my memory")
             
     def show_cmd(self):
         if self.data.get(self.name) is None:
-            self.taskdone("there's no " + self.name + " in the database")
+            self.taskdone("there's no " + self.name + " in my memory")
         else:
             #cv2.imshow("shirt of " + self.name, self.data[self.name]['body'])
             #cv2.imshow("pant of " + self.name, self.data[self.name]['pant'])
             #cv2.waitKey(15)
             img = self.data[self.name]['person']
-            self.taskdone("here is " + self.name)
+            self.taskdone("Im my memory, this is " + self.name)
             self.display(img, [], draw_box=False )
 
-    def display(self, img, box, draw_box = True):
+    def display(self, img, box, draw_box = True, text=None):
         if draw_box:
             xmin,xmax,ymin,ymax = box
             rect = (xmin,ymin,xmax-xmin,ymax-ymin)
-            draw_rectangle(img, rect)
+            draw_rectangle(img, rect, text=text)
+        imgmsg = self.bridge.cv2_to_imgmsg(img, encoding="bgr8")
+        self.display_pub.publish(imgmsg)
+
+    def display_status(self, img, known, unknown):
+        draw_status(img, known, unknown)
         imgmsg = self.bridge.cv2_to_imgmsg(img, encoding="bgr8")
         self.display_pub.publish(imgmsg)
 
@@ -281,23 +314,30 @@ class Run:
             person = self.get_personboxes(required_all_points = False)[who_raised_hand]
 
             if body is not None and pant is not None:
-                if self.data.get(self.name) is None:
-                    self.taskdone("Hello " + self.name + " How are you")
-                    self.data[self.name] = {}
-                else:
-                    self.taskdone("Hello " + self.name + " we meet again")
-                self.voice_once = False
+                xmin1,xmax1,ymin1,ymax1 = body
+                xmin2,xmax2,ymin2,ymax2 = pant
+                w1 = xmax1 - xmin1
+                w2 = xmax2 - xmin2
+                h1 = ymax1 - ymin1
+                h2 = ymax2 - ymin2
+                if w1>BOX_MIN and w2>BOX_MIN and h1>BOX_MIN and h2>BOX_MIN:
+                    if self.data.get(self.name) is None:
+                        self.taskdone("Hello " + self.name + " How are you")
+                        self.data[self.name] = {}
+                    else:
+                        self.taskdone("Hello " + self.name + " we meet again")
+                    self.voice_once = False
 
-                img = self.image
-                xmin,xmax,ymin,ymax = body
-                self.data[self.name]['body'] = img[ymin:ymax, xmin:xmax]
-                xmin,xmax,ymin,ymax = pant
-                self.data[self.name]['pant'] = img[ymin:ymax, xmin:xmax]
+                    img = self.image
+                    self.data[self.name]['body'] = img[ymin1:ymax1, xmin1:xmax1]
+                    self.data[self.name]['pant'] = img[ymin2:ymax2, xmin2:xmax2]
 
-                xmin,xmax,ymin,ymax = person
-                self.data[self.name]['person'] = img[ymin:ymax, xmin:xmax]
+                    xmin,xmax,ymin,ymax = person
+                    self.data[self.name]['person'] = img[ymin:ymax, xmin:xmax]
 
-                self.display(img, person)
+                    import copy
+                    imgcopy = copy.deepcopy(img)
+                    self.display(imgcopy, person, text=self.name)
 
                 # Show (for debug)
                 #cv2.imshow("shirt", self.data[self.name]['body'])
@@ -310,43 +350,87 @@ class Run:
         # else:
         #     print("Please raise your hand")
 
-    def who_cmd(self):
-        if not self.voice_once:
-            self.voice("Can you please raise your hand?")
-            self.voice_once = True
+    # def who_cmd(self):
+    #     if not self.voice_once:
+    #         self.voice("Can you please raise your hand?")
+    #         self.voice_once = True
 
-        who_raised_hand = self.check_hand_raised()
-        if who_raised_hand is not None:
-            body = self.get_bodyboxes(required_all_points = True)[who_raised_hand]
-            pant = self.get_pantboxes(required_all_points = True)[who_raised_hand]
+    #     who_raised_hand = self.check_hand_raised()
+    #     if who_raised_hand is not None:
+    #         body = self.get_bodyboxes(required_all_points = True)[who_raised_hand]
+    #         pant = self.get_pantboxes(required_all_points = True)[who_raised_hand]
+    #         person = self.get_personboxes(required_all_points = False)[who_raised_hand]
 
+    #         if body is not None and pant is not None:
+    #             xmin1,xmax1,ymin1,ymax1 = body
+    #             xmin2,xmax2,ymin2,ymax2 = pant
+    #             img = self.image
+    #             unknowbody_pic = img[ymin1:ymax1, xmin1:xmax1]
+    #             unknowpant_pic = img[ymin2:ymax2, xmin2:xmax2]
+
+    #             names = self.data.keys()
+    #             sim_list=[]
+    #             for p in names:
+    #                 body_sim = get_sim(unknowbody_pic, self.data[p]['body'])
+    #                 pant_sim = get_sim(unknowpant_pic, self.data[p]['pant'])
+    #                 sim_list.append(body_sim + pant_sim)
+
+    #             mostlikely_sim = max(sim_list)
+
+    #             if mostlikely_sim < 1.2:
+    #                 mostlikely_name = None
+    #                 self.taskdone("I have not meet you before")
+    #             else:
+    #                 mostlikely_name = names[sim_list.index(mostlikely_sim)] 
+    #                 self.taskdone("I think your name is " + mostlikely_name)
+    #             self.voice_once = False
+    #             self.display(img, person, text=mostlikely_name)
+
+    def who_cmd2(self):
+        bodies = self.get_bodyboxes(required_all_points = True)
+        pants = self.get_pantboxes(required_all_points = True)
+        persons = self.get_personboxes(required_all_points = False)
+
+        known_counter = 0
+        unknown_counter = 0
+
+        img = self.image
+
+        for i in range(len(bodies)):
+            body = bodies[i]
+            pant = pants[i]
+            person = persons[i]
             if body is not None and pant is not None:
                 xmin1,xmax1,ymin1,ymax1 = body
                 xmin2,xmax2,ymin2,ymax2 = pant
-                img = self.image
                 unknowbody_pic = img[ymin1:ymax1, xmin1:xmax1]
                 unknowpant_pic = img[ymin2:ymax2, xmin2:xmax2]
 
                 names = self.data.keys()
-                sim_list=[]
-                for p in names:
-                    body_sim = get_sim(unknowbody_pic, self.data[p]['body'])
-                    pant_sim = get_sim(unknowpant_pic, self.data[p]['pant'])
-                    sim_list.append(body_sim + pant_sim)
+                mostlikely_sim = 0
 
-                mostlikely_sim = max(sim_list)
+                if names:
+                    sim_list=[]
+                    for p in names:
+                        body_sim = get_sim(unknowbody_pic, self.data[p]['body'])
+                        pant_sim = get_sim(unknowpant_pic, self.data[p]['pant'])
+                        sim_list.append(body_sim + pant_sim)
+                    mostlikely_sim = max(sim_list)
 
                 if mostlikely_sim < 1.2:
-                    self.taskdone("I have not meet you before")
+                    self.display(img, person, text="?")
+                    unknown_counter += 1
                 else:
-                    mostlikely_name = names[sim_list.index(mostlikely_sim)] 
-                    self.taskdone("I think you are " + mostlikely_name)
-                self.voice_once = False
+                    mostlikely_name = names[sim_list.index(mostlikely_sim)]     
+                    self.display(img, person, text=mostlikely_name)
+                    known_counter += 1
+
+        self.display_status(img,known_counter,unknown_counter)
 
     def find_cmd(self):
         img = self.image
-        bodies = self.get_bodyboxes(required_all_points=False)
-        pants = self.get_pantboxes(required_all_points=False)
+        bodies = self.get_bodyboxes(required_all_points=True)
+        pants = self.get_pantboxes(required_all_points=True)
         persons = self.get_personboxes(required_all_points=False)
 
         mostlikely_sim = []
@@ -378,18 +462,99 @@ class Run:
 
         #print(mostlikely_name)
         #print(mostlikely_sim)
+                if max(sim_list) < 1.2:
+                    self.display(img, persons[i], text="?")
+                else:
+                    mostlikely_name_str = names[sim_list.index(max(sim_list))]
+                    self.display(img, persons[i], text=mostlikely_name_str)
             
         if self.name in mostlikely_name:
             idx = mostlikely_name.index(self.name)
             sim = mostlikely_sim[idx]
             if sim > 1.4:
                 self.taskdone("I found " + self.name + " here")
-                self.display(img, persons[idx])
+                self.display(img, persons[idx], text=self.name)
                 # Show person
                 #xmin3,xmax3,ymin3,ymax3 = persons[idx]
                 #cv2.imshow("found person", img[ymin3:ymax3, xmin3:xmax3])
-                #cv2.waitKey(15)        
+                #cv2.waitKey(15)  
+    
+    def goCircle(self, secs):
+        rospy.loginfo("Go circle for {0:d} secs".format(secs))
+        # Time minus Duration is a Time
+        secs_from_now = rospy.Time.now() + rospy.Duration(secs)
 
+        while not rospy.is_shutdown():
+            if rospy.Time.now() < secs_from_now:
+                self.cmd_vel_pub.publish(genTwist(0, 0, 0, 0, 0, -1))
+            else:
+                rospy.loginfo("Stop turlebot")
+                self.cmd_vel_pub.publish(genTwist(0, 0, 0, 0, 0, 0))
+                break 
+
+    def move(self): 
+        vel_msg = Twist()
+
+        speed_line = 1.0
+        speed_rotate = 30.0 #degree/sec
+        angle = 180.0
+        distance = 3.0
+        isForward = 1#True or False
+        clockwise = 1  
+
+        #Converting from angles to radians
+        angular_speed = speed_rotate*2*PI/360
+        relative_angle = angle*2*PI/360
+
+        vel_msg.linear.x = 0
+        vel_msg.linear.y = 0
+        vel_msg.linear.z = 0
+        vel_msg.angular.x = 0
+        vel_msg.angular.y = 0
+
+        # Checking if our movement is CW or CCW
+        if clockwise:
+            vel_msg.angular.z = -abs(angular_speed)
+        else:
+            vel_msg.angular.z = abs(angular_speed)
+
+        t0 = rospy.Time.now().to_sec()
+        current_angle = 0
+
+        #self.cmd_vel_pub.publish(vel_msg)
+        while(current_angle < relative_angle):
+            self.cmd_vel_pub.publish(vel_msg)
+            t1 = rospy.Time.now().to_sec()
+            current_angle = angular_speed*(t1-t0)
+
+        #Forcing our robot to stop
+        vel_msg.angular.z = 0
+        self.cmd_vel_pub.publish(vel_msg)
+        #rospy.spin()
+        #Checking if the movement is forward or backwards
+        # if(isForward):
+        #     vel_msg.linear.x = abs(speed_line)
+        # else:
+        #     vel_msg.linear.x = -abs(speed_line)
+        
+        # #Setting the current time for distance calculus
+        # t0 = rospy.Time.now().to_sec()
+        # current_distance = 0
+
+        # #Loop to move the turtle in an specified distance
+        # while(current_distance < distance):
+        #     #Publish the velocity
+        #     self.cmd_vel_pub.publish(vel_msg)
+        #     #Takes actual time to velocity calculus
+        #     t1=rospy.Time.now().to_sec()
+        #     #Calculates distancePoseStamped
+        #     current_distance= speed_line*(t1-t0)
+        # #After the loop, stops the robot
+        # vel_msg.linear.x = 0
+        # #Force the robot to stop
+        # self.cmd_vel_pub.publish(vel_msg)
+
+        self.taskdone('Done Move')
 
     def react_to_command(self):
         names = self.data.keys()
@@ -397,6 +562,7 @@ class Run:
         if self.command == 'list':
             self.display_cam = True
             self.list_cmd()
+            self.display_cam = True
 
         elif self.command == 'show':
             self.display_cam = True
@@ -407,11 +573,13 @@ class Run:
             self.meet_cmd()
 
         elif self.command == 'who':
-            self.display_cam = True
-            if names:
-                self.who_cmd() 
-            else:
-                self.taskdone("I have not meet anyone yet")
+            #self.display_cam = True
+            self.who_cmd2() 
+            # if names:
+            #     self.who_cmd() 
+            # else:
+            #     self.taskdone("I have not meet anyone yet")
+            #     self.display_cam = True
 
         elif self.command == 'find':
             self.display_cam = True
@@ -419,7 +587,12 @@ class Run:
                 self.find_cmd()
             else:
                 self.taskdone("I have not meet " + self.name + ' before')
-                
+                self.display_cam = True
+
+        elif self.command == 'move':
+            self.move()
+            #self.goCircle(3)
+
 
 def send_cmd_loop(app):
     while True:
